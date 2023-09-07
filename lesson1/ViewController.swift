877ff7fe58adbf84e17bdf6530c6d09f6c96bb86
Olimdzhon <write2sd@mail.ru>
2023-09-07 17:01:35 +0500

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Олимджон Садыков on 07/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
