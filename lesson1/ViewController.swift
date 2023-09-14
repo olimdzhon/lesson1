5f2ccfa6df80a86f1ce04c4a7994979a9c183209
Olimdzhon <write2sd@mail.ru>
2023-09-14 09:41:00 +0500

    added button click;

--- lesson1/ViewController.swift
+++ lesson1/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+        var count = 0
+        count = 2 + 2
+        
+        Label.text = "Answer: \(count)"
+    }
+    
 }
 
